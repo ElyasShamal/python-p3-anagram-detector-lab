f18e5ad54a55857ede5728eeee32a53deb6f7783
Elyas <Elyas.shamal2@gmail.com>
2023-08-05 17:33:47 -0400

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words_list):    
+        anagrams = []
+        sort_word = sorted(self.word.lower())
+
+        for word in words_list:
+            if sort_word == sorted(word.lower()) and self.word.lower() != word.lower():
+                anagrams.append(word)
+
+        return anagrams        
